7d7bb8577de0d817bd42ace255dedd581f8bf4c9
vikash kumar D <vikash.d.kumar@ericsson.com>
2018-06-24 23:35:29 +0530

    merge Test

--- SCProject/src/main/scala/01.Hello.scala
+++ SCProject/src/main/scala/01.Hello.scala
@@ -1,19 +1,23 @@
 object Test {
   //object oriented approach:-
   def main(args:Array[String]){
-    println("Hello Scala");
+    println("Hello");
     scalaExample
-    scalaExample1
+    scalaExample1;scalaExample2
   }
 
   //functional approach:-
   def scalaExample{
-    println("Hello Scala world")
+    println("Hello Scala ")
   }
 
   //functional approach:-
   def scalaExample1{
     println("Hello Scala world")
+  }
+
+  def scalaExample2{
+    println("Hello Scala world merge")
   }
 }
 
